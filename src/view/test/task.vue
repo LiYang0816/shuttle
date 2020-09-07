0cf2fe6bf962dc03f5f3e6b6ddd1f0693e8e3c7f
02029022 <02029022@yto.net.cn>
2020-09-07 09:22:19 +0800

    提交一个我在写项目时遇到的问题

---
+++ src/view/test/task.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="test">{{test}}</div>
+</template>
+
+<script>
+export default {
+  name: '',
+  data () {
+    return {
+      test: '',
+      count: 0
+    }
+  },
+  components: {},
+  mounted () {
+    this.count = 2;
+    this.getData();
+    this.count = 1;
+  },
+  methods: {
+    getData () {
+      this.test = this.count;
+    }
+  }
+}
+</script>
+
+<style>
+</style>
